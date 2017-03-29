f62f70a72101854ce0a17b9a2aa5197f6119f62c
Ryan James <rcjames1004@gmail.com>
2017-03-28 20:56:20 -0700

    Remove name from navbar

--- src/shared/component/nav.jsx
+++ src/shared/component/nav.jsx
@@ -23,7 +23,6 @@ const Nav = () =>
 					<span className="icon-bar"></span>
 					<span className="icon-bar"></span>
 				</button>
-				<a className="navbar-brand" href="#">Danielle</a>
 			</div>
 
 			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
